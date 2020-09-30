44746bdbfaab48332d14fb1c229f4953afeec20f
dimasarf98 <67794162+dimasarf98@users.noreply.github.com>
2020-09-30 22:37:03 +0700

    added route and transaksi controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,54 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware('auth')->prefix('deafcare')->name('deafcare.')->group(function ()
+{
+    Route::prefix('kesehatan')->name('kesehatan.')->group(function()
+    {
+        Route::resource('jenis', 'JenisKesehatanController', [
+            'only' => ['index','show']
+        ]);
+        Route::resource('tenagaKesehatan', 'TenagaKesehatanController', [
+            'only' => ['index','show']
+        ]);
+
+    });
+
+    Route::prefix('forum')->name('forum.')->group(function()
+    {
+        Route::resource('komunitas', 'KomunitasController', [
+            'only' => ['index','show']
+        ]);
+        Route::resource('sekolah', 'SekolahController', [
+            'only' => ['index','show']
+        ]);
+    });
+
+    Route::prefix('informasi')->name('informasi.')->group(function()
+    {
+        Route::resource('artikel', 'ArtikelController', [
+            'only' => ['index','show']
+        ]);
+    });
+
+    Route::prefix('belanja')->name('belanja.')->group(function()
+    {
+        Route::resource('item', 'BelanjaController', [
+            'only' => ['index','show']
+        ]);
+        Route::resource('transaksi', 'TransaksiController');
+    });
+
+    Route::prefix('tespendengaran')->name('tespendengaran.')->group(function()
+    {
+        Route::resource('hearingcenter', 'HearingCenterController', [
+            'only' => ['index','show']
+        ]);
+
+        Route::resource('rumahsakit', 'RumahSakitController', [
+            'only' => ['index','show']
+        ]);
+    });
+    
+});
