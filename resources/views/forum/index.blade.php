20410d1f69cb54f4884a4b8cb1f991824b32698d
Albarra Erdanto <albarraerdanto@gmail.com>
2020-10-01 11:05:05 +0700

    Membersihkan kodingan

--- resources/views/forum/index.blade.php
+++ resources/views/forum/index.blade.php
@@ -46,62 +46,6 @@
                     </div>
                 </div>
             </div>
-            <div class='card rounded-xl link-style-none mb-3' href="#">
-                <div class="card-body d-flex">
-                    <img class="rounded mr-3" src="{{asset('img/dummy.jpg')}}" alt="..." style="width: 82px; height:82px; object-fit:cover">
-                    <div class="flex-fill d-flex flex-column justify-content-between" style="min-width: 0">
-                        <div>
-                            <p class="font-weight-bold mb-1 text-truncate" style="line-height:1rem">
-                                Deaf Art Community: Menginspirasi Seperti Kupu-Kupu
-                            </p>
-                            <small class="mb-0">
-                                <div class="d-flex align-items-center">
-                                    <i class="fas fa-map-marker-alt mr-3"></i>
-                                    <div>
-                                        Jl. Langenarjan Lor No.16 A, Panembahan, Yogyakarta
-                                    </div>
-                                </div>
-                            </small>
-                            <small class="mb-0">
-                                <div class="d-flex align-items-center">
-                                    <i class="fas fa-phone-alt mr-3"></i>
-                                    <div>
-                                        082251919416
-                                    </div>
-                                </div>
-                            </small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class='card rounded-xl link-style-none mb-3' href="#">
-                <div class="card-body d-flex">
-                    <img class="rounded mr-3" src="{{asset('img/dummy.jpg')}}" alt="..." style="width: 82px; height:82px; object-fit:cover">
-                    <div class="flex-fill d-flex flex-column justify-content-between" style="min-width: 0">
-                        <div>
-                            <p class="font-weight-bold mb-1 text-truncate" style="line-height:1rem">
-                                Deaf Art Community: Menginspirasi Seperti Kupu-Kupu
-                            </p>
-                            <small class="mb-0">
-                                <div class="d-flex align-items-center">
-                                    <i class="fas fa-map-marker-alt mr-3"></i>
-                                    <div>
-                                        Jl. Langenarjan Lor No.16 A, Panembahan, Yogyakarta
-                                    </div>
-                                </div>
-                            </small>
-                            <small class="mb-0">
-                                <div class="d-flex align-items-center">
-                                    <i class="fas fa-phone-alt mr-3"></i>
-                                    <div>
-                                        082251919416
-                                    </div>
-                                </div>
-                            </small>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 </main>
